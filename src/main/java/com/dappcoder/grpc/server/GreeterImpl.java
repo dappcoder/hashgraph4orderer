120cd8999a049bdbde7ff4c62832f018508ead51
Alex Males <alexandru.males@gmail.com>
2018-06-07 22:42:58 +0300

    Async consensus response back to client

--- src/main/java/com/dappcoder/grpc/server/GreeterImpl.java
+++ src/main/java/com/dappcoder/grpc/server/GreeterImpl.java
@@ -12,6 +12,8 @@ public class GreeterImpl extends GreeterGrpc.GreeterImplBase implements Consensu
 
     private final List<GrpcServer.MessageHandler> messageHandlers = new ArrayList<>();
 
+    private final List<RegistryEntry> registry = new ArrayList<>();
+
     public void addMessageHandler(GrpcServer.MessageHandler handler) {
         messageHandlers.add(handler);
     }
@@ -19,15 +21,38 @@ public class GreeterImpl extends GreeterGrpc.GreeterImplBase implements Consensu
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
         messageHandlers.forEach(handler -> handler.handle(req.getName()));
-
-        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
-
-        responseObserver.onNext(reply);
-        responseObserver.onCompleted();
+        registry.add(new RegistryEntry(responseObserver, req));
     }
 
     @Override
     public void handle(String consensus) {
         System.out.println("todo: BUILDING REPLY WITH CONSENSUS");
+
+        registry.forEach(entry -> {
+            HelloReply reply = HelloReply.newBuilder().setMessage("CONSENSUS Hello " + entry.getRequest().getName()).build();
+            entry.getObserver().onNext(reply);
+            entry.getObserver().onCompleted();
+        });
+
+    }
+
+    class RegistryEntry {
+
+        private StreamObserver<HelloReply> observer;
+
+        private HelloRequest request;
+
+        public RegistryEntry(StreamObserver<HelloReply> observer, HelloRequest request) {
+            this.observer = observer;
+            this.request = request;
+        }
+
+        public StreamObserver<HelloReply> getObserver() {
+            return observer;
+        }
+
+        public HelloRequest getRequest() {
+            return request;
+        }
     }
 }
